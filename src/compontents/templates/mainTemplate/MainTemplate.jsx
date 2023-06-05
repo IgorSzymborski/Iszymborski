dc082c6214e6edacf0e942930512053f0c0b1aea
Igor <igor.szymborski98@gmail.com>
2023-06-05 19:12:42 +0200

    change format of background photo

--- src/compontents/templates/mainTemplate/MainTemplate.jsx
+++ src/compontents/templates/mainTemplate/MainTemplate.jsx
@@ -4,7 +4,7 @@ import HomePage from '../homePage/HomePage';
 import AboutMe from '../aboutMe/AboutMe';
 import MyProjects from '../portfolio/Portfolio';
 import Contact from '../contact/Contact';
-import me from '../../../assets/me2.webp';
+import me from '../../../assets/main1-min.png';
 import styles from '../mainTemplate/MainTemplate.module.scss';
 import {
   BrowserRouter,
